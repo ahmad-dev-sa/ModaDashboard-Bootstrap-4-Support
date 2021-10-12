1b16b52930e08aa582638e079c1ed2dbfcbbe357
ahmad alonazi <59384646+ahmad-dev-sa@users.noreply.github.com>
2021-10-12 10:54:10 +0300

    Starting version of the template

---
+++ js/dashboard.js
@@ -0,0 +1,101 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  var ctx = document.getElementById('myChart');
+  var ctx2 = document.getElementById('myChart2')
+
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'الأحد',
+        'الإثنين',
+        'الثلاثاء',
+        'الأربعاء',
+        'الخميس',
+        'الجمعة',
+        'السبت'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#344f4d',
+        borderWidth: 4,
+        pointBackgroundColor: '#344f4d'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+
+  var myChart2 = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+      labels: [
+        'الأحد',
+        'الإثنين',
+        'الثلاثاء',
+        'الأربعاء',
+        'الخميس',
+        'الجمعة',
+        'السبت'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: '#344f4d',
+        borderColor: '#344f4d',
+        borderWidth: 4,
+        pointBackgroundColor: '#344f4d'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+
+
+
+})()
